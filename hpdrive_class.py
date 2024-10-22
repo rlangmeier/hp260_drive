1c92bffbcd8b49405412d408f0334214b9763b5f
Langmeier Robert <robert.langmeier@mobi.ch>
2024-10-22 20:51:53 +0200

    Work in progress

--- hpdrive_class.py
+++ hpdrive_class.py
@@ -7,23 +7,30 @@ Created on Mon Oct 21 22:06:13 2024
 
 
 from pathlib import Path
+import struct
 
 
-# %%
+# %% 
 
 class Media:
     
     def __init__(self, file_name):
-        if isinstance(file_name, Path):
-            self.file_name = file_name
-        else:
-            self.file_name = Path(file_name)
+        self.file_name = Path(file_name)
         if not self.file_name.exists():
             raise FileExistsError()
-    
+        self._blocks = []
+        
+    def _read_blocks(self, block_size=256):
+        with self.file_name.open(mode='rb') as f:
+            while buf := f.read(block_size):
+                self._blocks.append(buf)
+        
+    def _system_block(self):
+        system_block = self._blocks[0]
+            
+        
     def mount(self):
-        pass
-    
+        self._read_blocks()
     
     def dismount(self):
         pass
@@ -40,4 +47,5 @@ if __name__ == "__main__":
     floppy_os_name = Path('./media').joinpath('250-OS-B.07.hpi')
 
     floppy_os = Media(floppy_os_name)
-    
+    floppy_os.mount()
+    struct.unpack()
